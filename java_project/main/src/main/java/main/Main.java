12db97da2c441c34f3f3ce8dc7f81a97b6dbb76e
Luca BORRUTO <luca.borruto@viacesi.fr>
2018-07-03 17:22:38 +0200

    pre-alpha

--- java_project/main/src/main/java/main/Main.java
+++ java_project/main/src/main/java/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import view.Game;
 
-
 public class Main {
 
 	public static void main(String[] args) {
--- java_project/view/src/main/java/view/GamePanel.java
+++ java_project/view/src/main/java/view/GamePanel.java
@@ -2,12 +2,8 @@ package view;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -17,29 +13,14 @@ public class GamePanel extends JPanel {
 
 	public static final int WIDTH = 618;
 	public static final int HEIGHT = 414;
-
-	BufferedImage img;
+	
 
 	public GamePanel() {
 		super();
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setFocusable(true);
 		requestFocus();
-		init();
-	}
-
-	private void init() {
-		try {
-			img = ImageIO.read(getClass().getResourceAsStream("/1.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void paintComponent(Graphics g) {
-		if (img != null) {
-			g.drawImage(img, 0, 0, null);
-			}
-
+		Init.init();
+		
 	}
 }
