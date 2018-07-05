f91c24006490f20eaf2659767c45f978d894804e
Luca BORRUTO <luca.borruto@viacesi.fr>
2018-07-05 17:33:28 +0200

    pre-config

---
+++ java_project/main/src/main/java/main/GamePanel.java
@@ -0,0 +1,27 @@
+package main;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class GamePanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH = 618;
+	public static final int HEIGHT = 414;
+
+	BufferedImage img;
+
+	public GamePanel() {
+		super();
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setFocusable(true);
+		requestFocus();
+	}
+}
